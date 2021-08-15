4178e4222a2775fb642fbf38da96f44a69b08e68
Maxim Oleynik <max2896@ya.ru>
2021-08-15 18:32:28 +0300

    Add new SplashScreen

--- app/src/main/kotlin/com/maxciv/healthcheck/MainActivity.kt
+++ app/src/main/kotlin/com/maxciv/healthcheck/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.maxciv.healthcheck.ui.theme.HealthCheckTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             HealthCheckTheme {
                 // A surface container using the 'background' color from the theme
